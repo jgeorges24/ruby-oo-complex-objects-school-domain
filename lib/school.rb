bc3bab628eb5e24e75f00a3e6d7ab5429340ba31
JEAN-BAPTISTE <jgeorges24@icloud.com>
2021-02-13 10:26:49 -0500

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,24 @@
 # code here!
+
+
+class School
+
+    def intitialize(name)
+        @name = name
+
+    end
+
+    def school=(school)
+        @school = school
+    end
+
+
+    def add_student(student_name, num)
+
+    end
+
+end
+
+
+ #schoolname = school.new("Bayside High School")
+
